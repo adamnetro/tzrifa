b94967914cb747a2cc9589b5457996dae5e3a496
adamnetro <64706091+adamnetro@users.noreply.github.com>
2023-05-07 00:40:57 +0100

    Add files via upload

--- virus X.bat
+++ virus X.bat
@@ -1,6 +1,35 @@
-cd Desktop
+cd downloads
 mkdir zrftk1 zrftk2 zrftk3 zrftk4 zrftk5 zrftk6 zrftk7 zrftk8 zrftk9 zrftk10 zrftk11 zrftk12 zrftk13 zrftk14 zrftk15 zrftk16 zrftk17 zrftk18 zrftk19 zrftk20 zrftk21 zrftk22 zrftk23 zrftk24 zrftk25 zrftk26 zrftk27 zrftk28 zrftk29 zrftk30 zrftk31 zrftk32 zrftk33 zrftk34 zrftk35 zrftk36 zrftk37 zrftk38 zrftk39 zrftk40 zrftk41 zrftk42 zrftk43 zrftk44 zrftk45 zrftk46 zrftk47 zrftk48 zrftk49 zrftk50 zrftk51 zrftk52 zrftk53 zrftk54 zrftk55 zrftk56 zrftk57 zrftk58 zrftk59 zrftk60 zrftk61 zrftk62 zrftk63 zrftk64 zrftk65 zrftk66 zrftk67 zrftk68 zrftk69 zrftk70 zrftk71 zrftk72 zrftk73 zrftk74 zrftk75 zrftk76 zrftk77 zrftk78 zrftk79 zrftk80 zrftk81 zrftk82 zrftk83 zrftk84 zrftk85 zrftk86 zrftk87 zrftk88 zrftk89 zrftk90 zrftk91 zrftk92 zrftk93 zrftk94 zrftk95 zrftk96 zrftk97 zrftk98 zrftk99 zrftk100
+rename 01000111.txt 01000111.mp3
+start 01000111.mp3
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
+start chrome
 color c
 dir/s
 msg * "System Error Data will be deleted by ' MrNetro ' after 1 min "
-shutdown -s -t 60
